9edfbb0bfe1c98d1193dbdc4607968c848e75007
Krishporwal011 <krishporwal486@gmail.com>
2026-04-27 19:43:34 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const PORT = env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`NODE_ENV=${env.NODE_ENV}`);
   console.log(`Frontend URL: ${env.FRONTEND_BASE_URL}`);
   if (env.ENABLE_IPFS) {
     console.log(`IPFS Upload: ENABLED`);
